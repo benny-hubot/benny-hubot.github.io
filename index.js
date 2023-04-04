bcd64d44e7354c793c471477c9385780cd2e7d37
willgraves1 <williamggraves@icloud.com>
2023-04-04 10:28:54 +0100

    created

---
+++ index.js
@@ -0,0 +1,8 @@
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "HGsysadminBE" && password == "R2D2/BB8"){
+alert ("Login successfully");
+window.location = "hannah/home.html";
+}
+}
